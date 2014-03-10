fb494d351770362069148ad9cbdc034aee6091ba
Jon Dutko <jonathan.dutko@tufts.edu>
2014-03-10 18:49:17 -0400

    basic flomp navigation
    flomp can move, flomp can jump. flomp cannot yet learn; flomp cannot yet
    love.

--- flomp/game.js
+++ flomp/game.js
@@ -10,13 +10,30 @@ var canvas, ctx, sheet,
 	flompWidth = 100,
 	flompHeight = 65,
 	currentSprite = flompA,
-	currentFace = flompLeft;
+	currentFace = flompLeft,
+	destinationX = 0,
+	isMoving = 0
+	flompVelocity = 0
+	isJumping = 0
+	flompUps = 0;
 	
 function play(){
 	canvas = document.getElementById('game');
 	ctx = canvas.getContext('2d');
 	sheet = document.getElementById('sprites');
+	document.onkeydown = keycheck;
 	setInterval(singleFrame, 1000/25); //draw a single frame 15fps (1000ms/15)
+}
+
+function keycheck(e){
+	if(e.keyCode == '32') jump();
+}
+
+function jump(){
+	if(isJumping == 0){
+		isJumping = 1;
+		flompUps = 10;
+	}
 }
 
 function singleFrame(){
@@ -25,11 +42,28 @@ function singleFrame(){
 }
 
 function clear() {
-	ctx.clearRect(0, 0, 800, 800);
+	ctx.clearRect(0, 0, 2000, 800);
 }
 
 function draw(){
 	drawFlomp();
+	//ctx.drawImage(sheet, 0, 400, 85, 70, 20, 20, 42, 35); //HP label
+}
+
+function checkFire(event){
+if(isMoving == 0){
+	isMoving = 1;
+}
+	var rect = canvas.getBoundingClientRect();
+	destinationX = event.clientX - rect.left;
+	if (destinationX < flompX){
+		currentFace = flompLeft;
+		flompVelocity = -4;
+	}
+	else{
+		currentFace = flompRight;
+		flompVelocity = 4;
+	}
 }
 
 function cycleFlomp(){
@@ -50,5 +84,23 @@ function cycleFlomp(){
 
 function drawFlomp(){
 	cycleFlomp();
+	if(isMoving == 1){
+		if(currentFace == flompLeft && flompX < destinationX){
+			isMoving = 0;
+		}
+		else if (currentFace == flompRight && flompX > (destinationX-flompWidth)){
+			isMoving = 0;
+		}
+		else{
+			flompX += flompVelocity;
+		}
+	}
+	if(isJumping == 1){
+		flompUps -= 1;
+		if(flompUps == -9){
+			isJumping = 0;
+		}
+		flompY -= flompUps;
+	}
 	ctx.drawImage(sheet, currentSprite, currentFace, flompWidth, flompHeight, flompX, flompY, flompWidth, flompHeight);
 }
