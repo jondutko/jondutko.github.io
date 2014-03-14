b2565cd54c2b3cf31c2a82e5521ebbee9b7fde69
Jon Dutko <jonathan.dutko@tufts.edu>
2014-03-14 18:25:49 -0400

    fixing haversine a new hope

--- transit/scripts/transit.js
+++ transit/scripts/transit.js
@@ -87,7 +87,7 @@ function haversine(stationLat, stationLng){
 	var dLon = x2.toRad();
 
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(stationLat.toRad) * Math.cos(mylat.toRad); 
 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 	var d = R * c;
 	return d;
