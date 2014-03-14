fe8d02938f1d43b1c3c0f25d9b32bcd96d541b82
Jon Dutko <jonathan.dutko@tufts.edu>
2014-03-14 17:18:44 -0400

    error 7

--- transit/scripts/transit.js
+++ transit/scripts/transit.js
@@ -1,12 +1,12 @@
 var map;
+var mapOptions = {
+  zoom: 8,
+  center: new google.maps.LatLng(-34.397, 150.644)
+};
+
 function initialize() {
   console.log("we got here");
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
