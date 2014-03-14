d2e39a6ef9ffd2a2a50a5157a0d93e4e18d07840
Jon Dutko <jonathan.dutko@tufts.edu>
2014-03-14 18:03:10 -0400

    initializing stations array

--- transit/scripts/transit.js
+++ transit/scripts/transit.js
@@ -6,6 +6,7 @@ var mOptions = {
 	center: me,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
 };
+var stations = [];
 var map;
 var currentMarker;
 var inforwindow = new google.maps.InfoWindow();
@@ -13,7 +14,13 @@ var places;
 
 function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'), mOptions);
+  initializeStations();
   findMe();
+}
+
+function initializeStations(){
+	stations.push({'line' : "Blue", 'name' : "Airport",
+		'lat' = 42.37426, 'lng' = -71.0304});
 }
 
 function findMe(){
@@ -27,6 +34,10 @@ function findMe(){
 	else{
 		alert("Geolocation is not supported by you internet browser.");
 	}
+}
+
+function calculateShortest(){
+
 }
 
 function updateMap(){
