ca62a49fb90b4f3d4675fd895a0ece2cd1d2be6e
Jon Dutko <jonathan.dutko@tufts.edu>
2014-03-14 17:35:24 -0400

    error 14

--- transit/scripts/transit.js
+++ transit/scripts/transit.js
@@ -1,11 +1,11 @@
-var map,
-mapOptions = {
+var mapOptions = {
   zoom: 8,
-  center: new google.maps.LatLng(-34.397, 150.644)
+  center: new google.maps.LatLng(-34.397, 150.644),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 
 function initialize() {
-  console.log("we got here!");
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  console.log("we got here! 2");
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
 
