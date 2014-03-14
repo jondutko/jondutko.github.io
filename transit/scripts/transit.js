e6a74b98bfbcf1d7490f819a1e7d1ff086edef66
Jon Dutko <jonathan.dutko@tufts.edu>
2014-03-14 17:40:02 -0400

    back on track again finally

--- transit/scripts/transit.js
+++ transit/scripts/transit.js
@@ -1,11 +1,16 @@
-var mapOptions = {
-  zoom: 8,
-  center: new google.maps.LatLng(-34.397, 150.644),
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-
+var lat, lng;
 function initialize() {
+  navigator.geolocation.getCurrentPosition(access);
+  var mapOptions = {
+	zoom: 8,
+	center: new google.maps.LatLng(lat, lng),
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
   console.log("we got here! 2");
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+function access(position){
+	lat = position.coords.latitude;
+	lng = position.coords.longitude;
 }
 
